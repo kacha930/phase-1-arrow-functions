598862b4561d463108088555b771d6f3e8be0e00
kacha930 <robiwere20@gmail.com>
2024-07-21 19:05:16 +0300

    success

---
+++ index.js
@@ -0,0 +1,15 @@
+//
+function divide(a,b) {
+    a = 2000;
+    b = 100;
+    return a/b
+}
+function square (x){
+    x = 2;
+    return x*x
+}
+function add(a,b){
+    a = 3;
+    b = 4;
+    return a+b
+}
